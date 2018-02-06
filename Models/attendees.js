e50924f1aa9ab4c48181ac970e280a7ea611580e
rockysingh3 <rockygsingh@icloud.com>
2018-02-06 15:13:28 -0500

    Connected to the db and created the schema

--- Models/attendees.js
+++ Models/attendees.js
@@ -1,1 +1,9 @@
-const mongoose = require('mongoose');
+'use strict'
+const config = require('../config');
+const Mongoose = require('mongoose').connect(config.dbURI);
+
+
+// check if the db is connected
+Mongoose.connection.on('error', error => {
+  console.log("MongoDB Error: ", error);
+});
