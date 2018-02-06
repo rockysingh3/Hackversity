1e8fadc7dc024b5ee5defce3f9bd7b6290bda7d9
rockysingh3 <rockygsingh@icloud.com>
2018-02-06 11:23:51 -0500

    Created High school student Model

--- Models/highSchoolStudentsModel.js
+++ Models/highSchoolStudentsModel.js
@@ -1,1 +1,9 @@
 const mongoose = require('mongoose');
+
+
+const HighSchoolStudents = new mongoose.Schema({
+
+});
+
+
+let HighSchoolStudentsModel = mongoose.model('HighSchoolStudents', HighSchoolStudents);
